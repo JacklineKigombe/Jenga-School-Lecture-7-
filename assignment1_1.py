2da79b560e86fd89d7484bdf349fcdd5561fd9b0
JacklineKigombe <muthoninamunyu@gmail.com>
2021-10-14 20:34:21 +0300

    Added two lecture7 assignment files previuosly stored in canvas

---
+++ assignment1_1.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello World!")
+    print("Hello Again")
+    print('Yay! Printing.')
+    print("I'd much rather you 'not'.")
+    print('I "said" do not touch this')
+
+main()
